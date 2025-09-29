057717045150eae97a67402e24141e1399165120
Aaron Vasquez <aaronvasquez9000@gmail.com>
2025-09-29 16:15:31 -0700

    Create variables.gd

---
+++ Variables/variables.gd
@@ -0,0 +1,17 @@
+# [Non-static type variables]
+var Username = "em.aaron" #String
+var Alive = true #Bool
+var Health = 6 #Int
+var Speed = 12.5 #Float
+
+# [Static type variables 1]
+var Username : String = "em.aaron"
+var Alive : bool = true
+var Health : int = 6
+var Speed : float = 12.5
+
+# [Static type variables 2]
+var Username : String = String("em.aaron")
+var Alive : bool = bool(true)
+var Health : int = int(6)
+var Speed : float = float(12.5)
